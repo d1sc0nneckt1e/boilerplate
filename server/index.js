aa5261ec56ae7426fda976f6627aa64b806afa89
Veritaz Lamagra <haehnchen.mike@gmail.com>
2021-12-27 15:18:30 +0100

    connection to MongoDataBase

--- server/index.js
+++ server/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const app = express();
 
@@ -6,6 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=> console.log(`Server started on port: ${PORT}`));
 
-app.get("/test", (req, res)=>{
-    res.send("it works");
+mongoose.connect(process.env.MDB_CONNECT, (err)=> {
+    if(err) return console.error(err);
+    console.log("Connected to MDB");
 });
