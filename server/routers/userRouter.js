b98c04dc45446d127651b57393aec32c6fec9c9c
d1sc0nneckt1e <a.c.rezai@gmail.com>
2021-12-27 19:25:51 +0100

    -UPDATE refactored if statements

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -5,23 +5,27 @@ router.post('/', async (req, res) => {
     try{
         const {email, password, passwordVarify} = req.body;
         // validation
-        if (!email || !password || !passwordVarify)
+        if (!email || !password || !passwordVarify) {
             return res.status(400).json({
                 errorMessage: 'Please enter all required fields.'
             });
-        if (password.length < 6)
+        }
+        if (password.length < 6) {
             return res.status(400).json({
                 errorMessage: 'Please enter a password with at least 6 charecters.'
             });
-        if (password.length !== passwordVarify)
+        }
+        if (password.length !== passwordVarify) {
             return res.status(400).json({
                 errorMessage: 'Please enter the same password.'
             });
+        } 
         const existingUser = await User.findOne({email: email});
-        if (existingUser)
+        if (existingUser) {
             return res.status(400).json({
                 errorMessage: 'An account with this email exists.'
             });
+        }
     } catch (err) {}
         console.error(err)
         res.status(500).send();
