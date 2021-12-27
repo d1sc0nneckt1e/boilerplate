225cdea1473cc82b7b2df8cb06045a7aa51b531c
unknown <a.c.rezai@gmail.com>
2021-12-27 15:18:33 +0100

    -ADD mongoose, dotenv; mongodb connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+// set up server
 const app = express();
-
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
 
-app.get("/test", (req, res) => {
-    res.send("It works!");
-})
+// connect to mongoDB
+mongoose.connect(process.env.MDB_CONNECT, (err) => {
+    if (err) return console.error(err);
+    console.log('Connect to MongoDB');
+});
